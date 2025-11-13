1410ca220c2a5ccd2bd9bad25c618e82b11163e6
Soufyan <elomranisoufyan@gmail.com>
2025-11-13 17:24:24 +0100

    to submit

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+let data = []
+async function fetchjson() {
+  try {
+    const response = await fetch('data.json');
+    data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error 404 :(', error);
+  }
+}
+fetchjson()
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+localStorage.setItem("students",JSON.stringify(data))
+// let arrsize = students.length ;
+const confirmButton = document.getElementById("confirmstudent");
+    confirmButton.addEventListener("click", () => {
+        Swal.fire({
+            title: "Student added",
+            text: "New student successfully to GenV class",
+            icon: "success"
+        });
+    });
+confirmButton.addEventListener("click",() =>{
+    let obggg =  {
+        fname : document.getElementById("fname").value,
+        lname : document.getElementById("lname").value,
+        age : document.getElementById("age").value,
+        email : document.getElementById("email").value,
+        pfpurl  : document.getElementById("pfpurl").value
+    }
+    data.push(obggg)
+    console.log(data)
 })
+
